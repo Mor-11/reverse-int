7ccd1482d341e5122675ce99c81b627c6f35b6e7
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-16 17:14:52 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = [],
+        a = Math.abs(n); //создаем пустой массив и делаем число положительным
+    let arrs = a.toString().split(""); //делаем число стракой, а дальше массивом
+    for (let i = arrs.length - 1; 0 <= i; i--) { //перебираем массив с конца и добавляем в массив
+        arr.push(arrs[i]);
+    }
+    return +arr.join(""); //дадаем массив числом
+};
